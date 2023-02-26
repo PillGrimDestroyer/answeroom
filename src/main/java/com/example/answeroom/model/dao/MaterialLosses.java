d3c05a571060d662e09bd1d9f0287993c2e877f9
Roman <megamega4@yandex.ru>
2023-02-26 19:52:46 +0600

    Добавил срок годности для карточки МУ

--- src/main/java/com/example/answeroom/model/dao/MaterialLosses.java
+++ src/main/java/com/example/answeroom/model/dao/MaterialLosses.java
@@ -3,11 +3,15 @@ package com.example.answeroom.model.dao;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import java.util.Date;
+
 @Data
 @Accessors(fluent = true)
 public class MaterialLosses {
 
     private String id;
     private Document document;
+    private Date expiredAt;
+    private Date registeredAt;
 
 }
