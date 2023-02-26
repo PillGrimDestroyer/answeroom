a424bba4bd7d6d4404a03bc7e255ec736982c2d0
Roman <megamega4@yandex.ru>
2023-02-26 19:53:07 +0600

    Добавил забытое поле с датой создания документа

--- src/main/java/com/example/answeroom/model/dao/Document.java
+++ src/main/java/com/example/answeroom/model/dao/Document.java
@@ -3,11 +3,14 @@ package com.example.answeroom.model.dao;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import java.util.Date;
+
 @Data
 @Accessors(fluent = true)
 public class Document {
 
     private String id;
     private String name;
+    private Date registeredAt;
 
 }
